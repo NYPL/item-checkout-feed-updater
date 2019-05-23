84ca2346a6afb04dfbb1c20be9b039cfeee54bf6
Daniel Appel <dan.s.appel@gmail.com>
2019-05-23 14:48:10 -0400

    Add skeleton for util tests

---
+++ spec/checkout_builder_util_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+
+describe CheckoutBuilderUtil do
+  describe '#map_item_type_to_coarse_item_type' do
+    it 'should map 55 to Book' do
+    end
+    it 'should map 6 to Image' do
+    end
+    it 'should map 13 to AV' do
+    end
+    it 'should map 128 to Device' do
+    end
+  end
+
+  describe '#location_type' do
+  end
+
+  describe '#initial_checkout_property_assignment' do
+  end
+
+  describe '#assign_isbn' do
+  end
+
+  describe '#checkout_bib_property_assignment' do
+  end
+
+  describe '#get_bib' do
+  end
+end

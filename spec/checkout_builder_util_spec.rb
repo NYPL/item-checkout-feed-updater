347ba91616cc2267b5ad8385042064b9521878be
Daniel Appel <dan.s.appel@gmail.com>
2019-05-23 15:17:25 -0400

    Add initial checkout builder tests

--- spec/checkout_builder_util_spec.rb
+++ spec/checkout_builder_util_spec.rb
@@ -4,16 +4,30 @@ require 'spec_helper'
 describe CheckoutBuilderUtil do
   describe '#map_item_type_to_coarse_item_type' do
     it 'should map 55 to Book' do
+      expect(CheckoutBuilderUtil.map_item_type_to_coarse_item_type(55)).to eq('Book')
     end
+    
     it 'should map 6 to Image' do
+      expect(CheckoutBuilderUtil.map_item_type_to_coarse_item_type(6)).to eq('Image')
     end
+
     it 'should map 13 to AV' do
+      expect(CheckoutBuilderUtil.map_item_type_to_coarse_item_type(13)).to eq('AV')
     end
+
     it 'should map 128 to Device' do
+      expect(CheckoutBuilderUtil.map_item_type_to_coarse_item_type(128)).to eq('Device')
     end
   end
 
   describe '#location_type' do
+    it 'should assign item types over 100 to Branch' do
+      expect(CheckoutBuilderUtil.location_type(100)).to eq('Branch')
+    end
+
+    it 'should assign item types under 100 to Research' do
+      expect(CheckoutBuilderUtil.location_type(99)).to eq('Research')
+    end
   end
 
   describe '#initial_checkout_property_assignment' do

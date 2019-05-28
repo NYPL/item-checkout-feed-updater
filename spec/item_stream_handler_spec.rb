3f021e31f34c38b2c2d03471ff306cb16497ec92
Daniel Appel <dan.s.appel@gmail.com>
2019-05-28 16:24:53 -0400

    Add tests for removing old ids

--- spec/item_stream_handler_spec.rb
+++ spec/item_stream_handler_spec.rb
@@ -2,6 +2,126 @@ require 'spec_helper'
 
 describe ItemStreamHandler do
 
+  item_stream_handler = nil
+  before(:each) do
+    item_stream_handler = ItemStreamHandler.new
+  end
+
+  describe "#add_checkout" do
+    test_checkout = {}
+    before(:each) do
+      allow(test_checkout).to receive(:id).and_return('test_checkout')
+      allow(test_checkout).to receive(:barcode).and_return('test_checkout')
+      allow(test_checkout).to receive(:title).and_return('test_checkout')
+    end
+
+    it 'should add the checkout to a new item_stream_handler' do
+      item_stream_handler.add_checkout test_checkout
+      expect(item_stream_handler.instance_variable_get(:@checkouts)).to include(test_checkout)
+    end
+
+    it 'should log messages' do
+      expect(Application.logger).to receive(:debug).twice
+      item_stream_handler.add_checkout test_checkout
+    end
+  end
+
+  describe "#constrain_size" do
+    it 'should result in an array with the given size if the array is big enough' do
+      test_array = [1,2,3,4,5]
+      item_stream_handler.constrain_size(test_array,2)
+      expect(test_array.size).to eq(2)
+    end
+
+    it 'should not change the array if the array is smaller than the given size' do
+      test_array = [1]
+      item_stream_handler.constrain_size(test_array, 2)
+      expect(test_array.size).to eq(1)
+    end
+  end
+
+  describe "#update_tally_if_necessary" do
+    item_type_tally = nil
+    fake_now = []
+
+    before(:each) do
+      item_type_tally = ItemTypeTally
+      allow(Time).to receive(:now).and_return(fake_now)
+      allow(fake_now).to receive(:day).and_return(2)
+    end
+
+    after(:each) do
+      ItemTypeTally = item_type_tally
+    end
+
+    it 'should update the ItemTypeTally if necessary' do
+      fake_then = []
+      allow(fake_then).to receive(:day).and_return(1)
+      ItemTypeTally = {
+        time: fake_then
+      }
+      item_stream_handler.update_tally_if_necessary
+      expect(ItemTypeTally[:time]).to eq(fake_now)
+      expect(ItemTypeTally[:tallies]).to eq({})
+      ItemTypeTally[:tallies][:tally] += 1
+      expect(ItemTypeTally[:tallies][:tally])
+    end
+
+    it 'should leave the ItemTypeTally unchanged if no change is required' do
+      fake_then = []
+      allow(fake_then).to receive(:day).and_return(2)
+
+      ItemTypeTally = {
+        time: fake_now
+      }
+      item_stream_handler.update_tally_if_necessary
+      expect(ItemTypeTally[:time]).to eq(fake_then)
+      expect(ItemTypeTally[:tallies]).to eq(nil)
+    end
+  end
+
+  describe "#update_count" do
+  end
+
+  describe "#remove_old_ids" do
+    it 'should remove exactly the expired ids' do
+      expire_time = (ENV["CHECKOUT_ID_EXPIRE_TIME"] ||= "10").to_i
+      allow(Time).to receive(:now).and_return(0)
+      id_hash = {
+        a: Time.now + expire_time + 1,
+        b: Time.now + expire_time + 1,
+        c: Time.now + expire_time - 1,
+        d: Time.now + expire_time - 1,
+      }
+      item_stream_handler.remove_old_ids(id_hash)
+      expect(id_hash[:a]).to_not eq(nil)
+      expect(id_hash[:b]).to_not eq(nil)
+      expect(id_hash[:c]).to_not eq(nil)
+      expect(id_hash[:d]).to_not eq(nil)
+    end
+  end
+
+  describe "#get_decoded_records" do
+  end
+
+  describe "#convert_record_to_checkout" do
+  end
+
+  describe "#is_duplicate" do
+  end
+
+  describe "#update_recent_ids" do
+  end
+
+  describe "#process_checkout" do
+  end
+
+  describe "#process_checkouts" do
+  end
+
+  describe "#clear_old_data" do
+  end
+
   describe '#item_is_checkout?' do
     it 'should reject a malformed item' do
       expect(ItemStreamHandler.new.item_is_checkout?(nil)).to eq(false)
